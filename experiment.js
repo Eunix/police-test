fa3c43f3b41892578fdd4b0bc39894896164467d
Evgeny Pavlov <evgeny@pavlov.name>
2013-10-15 00:15:02 +0400

    Working experiment

--- experiment.js
+++ experiment.js
@@ -12,32 +12,32 @@ var sets = {
     10: [0, 1, 2, 3, 4, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 4, 3, 2, 1, 0]
   },
   noise: {
-    1:  ["IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB"],
-    2:  ["GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM"],
-    3:  ["IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB"],
-    4:  ["GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM"],
-    5:  ["IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB"],
-    6:  ["GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM"],
-    7:  ["IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB"],
-    8:  ["GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM"],
-    9:  ["IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB"],
-    10: ["GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM"],
-    11: ["IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB"],
-    12: ["GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM"],
-    13: ["IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB"],
-    14: ["GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM"],
-    15: ["IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB"],
-    16: ["GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM"],
-    17: ["IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB"],
-    18: ["GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM"],
-    19: ["IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB"],
-    20: ["GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM", "GB", "GB", "IM", "IM"]
+    1:  ["IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB"],
+    2:  ["GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN"],
+    3:  ["IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB"],
+    4:  ["GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN"],
+    5:  ["IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB"],
+    6:  ["GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN"],
+    7:  ["IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB"],
+    8:  ["GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN"],
+    9:  ["IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB"],
+    10: ["GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN"],
+    11: ["IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB"],
+    12: ["GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN"],
+    13: ["IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB"],
+    14: ["GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN"],
+    15: ["IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB"],
+    16: ["GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN"],
+    17: ["IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB"],
+    18: ["GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN"],
+    19: ["IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB"],
+    20: ["GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN", "GB", "GB", "IN", "IN"]
   }
 }
 
 window.set_number  = 1;
 window.step = 1;
-window.csv  = 'Набор,Номер изображения,Ответ';
+window.csv  = 'Набор,Номер изображения,Шум,Степень,Ответ';
 
 $(".set_button").on('click', function(e) {
   $(".new").addClass("hide");
@@ -58,7 +58,7 @@ $(document).on('click', ".answer img", function(e) {
 
   // Выбранный ответ
   chosen_answer = $(this).attr("data-number");
-  window.csv += "\n" + window.set_number + "," + window.step + "," + chosen_answer
+  window.csv += "\n" + window.set_number + "," + window.step + "," + current_noise() + "," + current_level() + "," + chosen_answer
 
   // Следующий шаг
   $(".answer").addClass("hide");
@@ -71,6 +71,7 @@ $(document).on('click', ".answer img", function(e) {
 $(".download_button").on("click", function(e) {
   var blob = new Blob([window.csv], {type: "text/plain;charset=utf-8"});
   saveAs(blob, "result_" + window.set_number + ".txt");
+  $(".reload_button").removeClass("hide");
 });
 
 $(".reload_button").on('click', function(e) {
@@ -80,17 +81,24 @@ $(".reload_button").on('click', function(e) {
 
   $(".thankyou").addClass("hide");
   $(".new").removeClass("hide");
+  $(".reload_button").addClass("hide");
   e.preventDefault();
 });
 
 function next_step() {
-  if(window.step < 5) {
+  if(window.step < 21) {
     // Preparation
-    $(".question img").attr("src", "images/" + window.step + "/0_" + + "_" + 0.jpg");
+    if(current_level() == 0) {
+      $(".question img").attr("src", "images/" + window.step + "/0.jpg");
+    } else {
+      $(".question img").attr("src", "images/" + window.step + "/0_" + current_noise() + "_" + current_level() + ".jpg");
+    }
+
     $(".answer img").each(function(index) {
       $(this).attr("src", "images/" + window.step + "/" + index + ".jpg");
       $(this).attr("data-number", index);
     });
+    $(".relax img").attr("src", "gifs/" + window.step + ".gif");
 
     $(".question").removeClass("hide");
 
@@ -110,9 +118,16 @@ function next_step() {
   }
 }
 
+function current_noise() {
+  return sets["noise"][window.set_number][(window.step - 1)];
+}
+
+function current_level() {
+  return sets["level"][window.set_number][(window.step - 1)];
+}
 $.fn.randomize = function(selector){
   var $elems = selector ? $(this).find(selector) : $(this).children(),
-    $parents = $elems.parent();
+  $parents = $elems.parent();
 
   $parents.each(function(){
     $(this).children(selector).sort(function(){
